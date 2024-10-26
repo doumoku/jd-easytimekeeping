e8728da6649772103a48af3b516fa9c0c1ed9d16
DC23 <jugglindan@gmail.com>
2024-10-26 20:26:09 +1100

    #4

--- module/settings.js
+++ module/settings.js
@@ -1,3 +1,16 @@
 const MODULE_ID = 'jd-dbtime'
 
-function registerSettings () {}
+function registerSettings () {
+    game.settings.register(MODULE_ID, 'showHourClock', {
+        name: 'Show Hours',
+        hint: 'Should the hour clock be shown?',
+        scope: 'world', 
+        config: true, 
+        type: Boolean,
+        default: false,
+        onChange: value => {
+            console.log(value)
+        },
+        requiresReload: true,
+    })
+}
