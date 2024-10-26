e538eac74f4355acac74411e7a3f2dd253ed571d
DC23 <jugglindan@gmail.com>
2024-10-26 20:04:19 +1100

    settings stub

--- module/dbtime.js
+++ module/dbtime.js
@@ -1,13 +1,12 @@
 /**
  * module registration functions
- * 
+ *
  * Version 1.0
  */
-
+import { registerSettings } from './settings.js'
 
 Hooks.once('init', () => {
-    // registerSettings();
-    // console.log('dbtime init')
+    registerSettings()
 })
 
 Hooks.once('setup', () => {
@@ -15,11 +14,10 @@ Hooks.once('setup', () => {
      * I probably want to create the clocks and setup the API here, since I need
      * Global Progress Clocks to be initialised first, and that happens in the init hook.
      */
-    // console.log('dbtime setup')
+    console.log('dbtime setup')
     // TODO: Something like this
     // game.modules.get('jd-dbtime').api = new DBTimeAPI()
 })
 
-Hooks.once('ready', () => {
-    // console.log('dbtime ready')
-})
+// Hooks.once('ready', () => {
+// })
