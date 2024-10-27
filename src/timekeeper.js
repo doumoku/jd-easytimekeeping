e1ecd6d0f6d3f0e353ecdd74a377c09ea7bd60d4
DC23 <jugglindan@gmail.com>
2024-10-27 12:57:00 +1100

    Stubs for public API of the timekeeper class #1

---
+++ src/timekeeper.js
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+// TODO: #1 bring over functionality from dbtime-engine.js prototype code
+export class Timekeeper {
+    constructor () {
+        // TODO: decide how much of the clock init should be done here, and how much done 
+        // outside with a dependency injection pattern
+    }
+
+    /**
+     * Increment the current time.
+     *
+     * @param {Number} stretches The number of stretches to increment.
+     */
+    async increment (stretches) {}
+
+    /**
+     * Set the time to the given total number of stretches since stretch 0 on day 0, which is 6am on day 0. This is
+     * mostly used by the Reset Clocks macro, though it can be used to set the time to any value so long as you calculate
+     * the total number of stretches required. There are 24 stretches per shift, 4 stretches per hour, 6 hours per shift,
+     * 4 shifts per day, and therefore 96 stretches per day.
+     *
+     * @param {Number} totalStretches The total number of 15 minute stretches since 6am on day 0.
+     */
+    async set (totalStretches = 0) {}
+
+    /**
+     * Posts the current time to chat.
+     * If the day clock is enabled in the module settings, then the day will be included in the chat message.
+     */
+    async tellTime () {}
+}
