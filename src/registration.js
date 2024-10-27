a411a49adf9e9cf158d71e1b470b09342e757be4
DC23 <jugglindan@gmail.com>
2024-10-27 15:26:15 +1100

    adding log groups to initialisation code

--- src/registration.js
+++ src/registration.js
@@ -1,12 +1,13 @@
 /**
- * module registration functions
+ * Dragonbane Timekeeping module registration functions
  *
- * Version 1.0
  */
 import { registerSettings } from './settings.js'
 
 Hooks.once('init', () => {
+    console.group('DB Time | init')
     registerSettings()
+    console.groupEnd()
 })
 
 Hooks.once('setup', () => {
@@ -14,9 +15,10 @@ Hooks.once('setup', () => {
      * I probably want to create the clocks and setup the API here, since I need
      * Global Progress Clocks to be initialised first, and that happens in the init hook.
      */
-    console.log('dbtime setup')
+    console.group('DB Time | setup')
 
     // game.modules.get('jd-dbtime').api = tick
+    console.groupEnd()
 })
 
 // Hooks.once('ready', () => {
