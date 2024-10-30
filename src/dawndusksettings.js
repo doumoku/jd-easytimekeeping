1764c95763cb76629d5adad828bedd9f5bcf87b1
DC23 <jugglindan@gmail.com>
2024-10-30 21:31:59 +1100

    implemented fixed choices for the start times of dawn and dusk. It's data driven so I can extend the choices later without needing to change the UI

--- src/dawndusksettings.js
+++ src/dawndusksettings.js
@@ -1,4 +1,4 @@
-import { MODULE_ID, SETTINGS } from "./settings.js"
+import { MODULE_ID, SETTINGS } from './settings.js'
 
 export function registerDawnDuskSettings () {
     // The settings menu
@@ -17,9 +17,11 @@ export function registerDawnDuskSettings () {
         config: false,
         type: Object,
         default: {
-            "dawn-dusk-enabled": false,
-            "day-darkness-level": 0,
-            "night-darkness-level": 1.0,
+            'dawn-dusk-enabled': false,
+            'day-darkness-level': 0,
+            'night-darkness-level': 1.0,
+            'sunset-start': '6:00 PM',
+            'dawn-start': '6:00 AM',
         },
     })
 }
@@ -32,9 +34,7 @@ class DawnDuskMenu extends FormApplication {
             width: 500,
             template: 'modules/jd-dbtime/templates/dawndusk.hbs',
             id: SETTINGS.DAWN_DUSK_MENU,
-            title: game.i18n.localize(
-                'DBTIME.Settings.DawnDuskConfig.name'
-            ),
+            title: game.i18n.localize('DBTIME.Settings.DawnDuskConfig.name'),
         })
     }
 
@@ -44,6 +44,27 @@ class DawnDuskMenu extends FormApplication {
             SETTINGS.DAWN_DUSK_SETTINGS
         )
 
+        // TODO: I could do better than this fixed choice of options for
+        // starting sunset and sunrise, but it's good enough for now
+        const sunsetOptions = {
+            '5:00 PM': false,
+            '5:30 PM': false,
+            '6:00 PM': false,
+            '6:30 PM': false,
+        }
+
+        const dawnOptions = {
+            '5:00 AM': false,
+            '5:30 AM': false,
+            '6:00 AM': false,
+            '6:30 AM': false,
+        }
+
+        // set the initial values for the form
+        sunsetOptions[initialValues['sunset-start']] = true
+        dawnOptions[initialValues['dawn-start']] = true
+        initialValues['sunset-start-options'] = sunsetOptions
+        initialValues['dawn-start-options'] = dawnOptions
         console.log(initialValues)
         return initialValues
     }
