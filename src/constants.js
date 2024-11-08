6f031ca95aceca4e88ab28c697e85098122dfa0f
DC23 <jugglindan@gmail.com>
2024-11-08 21:29:45 +1100

    removed ticks from constants ctor

--- src/constants.js
+++ src/constants.js
@@ -3,19 +3,11 @@
  */
 
 export class Constants {
-    constructor (tickDurationMinutes) {
-        console.assert(
-            60 % tickDurationMinutes === 0,
-            'tickDurationMinutes must evenly subdivide an hour'
-        )
-        this.minutesPerTick = tickDurationMinutes
+    constructor () {
         this.minutesPerDay = 1440
         this.shiftsPerDay = 4
         this.hoursPerShift = 6
-        this.maxDaysTracked = 128 // while we could track more, this is a limitation from GPC
         this.hoursPerDay = 24
-        this.ticksPerHour = Math.floor(60 / tickDurationMinutes)
-        this.ticksPerShift = this.ticksPerHour * this.hoursPerShift
-        this.ticksPerDay = this.ticksPerShift * this.shiftsPerDay
+        this.minutesPerShift = 60 * 6
     }
 }
