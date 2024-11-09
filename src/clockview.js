fc27db35eb0b7877a1b173d48e41398ee5b20209
DC23 <jugglindan@gmail.com>
2024-11-09 14:33:26 +1100

    implements 12/24 hour tell time handling

--- src/clockview.js
+++ src/clockview.js
@@ -31,27 +31,27 @@ export class ClockView {
         })
     }
 
-    get showHours () {
-        return game.settings.get(MODULE_ID, SETTINGS.SHOW_HOURS)
-    }
-
-    get showDays () {
-        return game.settings.get(MODULE_ID, SETTINGS.SHOW_DAYS)
-    }
-
     #checkAutoTellTime (time) {
         const tellTimeSettings = game.settings.get(MODULE_ID, SETTINGS.AUTO_TELL_TIME_SETTINGS)
-        const timeOfDay = this.#toTimeOfDay(time)
+        const timeOfDay = this.#toTimeOfDay(time, true)
         if (tellTimeSettings[timeOfDay]) this.tellTime(time)
     }
 
-    #toTimeOfDay (time) {
-        // TODO: once I implement 24-hour display module settings, I'll need a way to force 12-hour strings for when this is called from #checkAutoTellTime
-        // TODO: module settings for 24-hour display
+    #toTimeOfDay (time, force12Hour = false) {
         // time.hour is a value from 0 to 23
-        const amPm = time.hour >= 12 ? 'PM' : 'AM'
-        let hour = time.hour > 12 ? time.hour - 12 : time.hour
-        if (hour === 0) hour = 12
-        return `${hour}:${time.minute.toString().padStart(2, '0')} ${amPm}`
+        if (force12Hour || !this.#is24HourDisplay) {
+            const amPm = time.hour >= 12 ? 'PM' : 'AM'
+            let hour = time.hour > 12 ? time.hour - 12 : time.hour
+            if (hour === 0) hour = 12
+            return `${hour}:${time.minute.toString().padStart(2, '0')} ${amPm}`
+        } else {
+            return `${time.hour.toString().padStart(2, '0')}:${time.minute
+                .toString()
+                .padStart(2, '0')}`
+        }
+    }
+
+    get #is24HourDisplay () {
+        return game.settings.get(MODULE_ID, SETTINGS.DISPLAY_24_HOUR_TIME)
     }
 }
