b42b71b52c2c6382bc9b4dd1d235de9ccf0c7fe0
DC23 <jugglindan@gmail.com>
2024-11-10 22:49:13 +1100

    I have a foundry themed panel that matches the size of the player panel and tracks position. Frameless. It renders the time already. Since I don't implement sockets I don't think it will work across clients, need to test that. But it's progress. Fixes #80

--- src/initialisation.mjs
+++ src/initialisation.mjs
@@ -12,10 +12,27 @@ import { UIPanel } from './uipanel.mjs'
 Hooks.once('init', () => {
     console.group('JD ETime | init')
     registerSettings()
+
+    /**
+     * This creates a DOM element in the ui-left interface div,
+     * in between the canvas controls and the players panel.
+     * */
+    createUIPanelElement('#controls')
+
     game.modules.get(MODULE_ID).uiPanel = new UIPanel()
     game.modules.get(MODULE_ID).uiPanel.init()
     console.groupEnd()
 })
+
+// Create a DOM element somewhere in the page. Technique from Global Progress Clocks
+function createUIPanelElement (insertAfter = '#ui-top') {
+    const top = document.querySelector(insertAfter)
+    if (top) {
+        const template = document.createElement('template')
+        template.setAttribute('id', UIPanel.ID)
+        top.insertAdjacentElement('afterend', template)
+    }
+}
 
 Hooks.once('ready', async () => {
     console.group('JD ETime | ready')
--- src/uipanel.mjs
+++ src/uipanel.mjs
@@ -6,19 +6,12 @@ import { MODULE_ID, SETTINGS } from './settings.mjs'
 const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api
 
 export class UIPanel extends HandlebarsApplicationMixin(ApplicationV2) {
+    static ID = 'jd-et-uipanel'
     static DEFAULT_OPTIONS = {
-        // tag: 'form',
-        id: 'jd-et-uipanel',
+        tag: 'div',
+        id: UIPanel.ID,
         window: {
-            frame: true,
-            resizable: true,
-            title: 'UI Panel',
-        },
-        position: {
-            top: 200,
-            left: 200,
-            width: 300,
-            height: 200,
+            frame: false,
         },
     }
 
@@ -29,6 +22,7 @@ export class UIPanel extends HandlebarsApplicationMixin(ApplicationV2) {
     }
 
     #time = {}
+    refresh = foundry.utils.debounce(this.render, 200)
 
     init () {
         Hooks.on(Timekeeper.TIME_CHANGE_HOOK, this.timeChangeHandler.bind(this))
@@ -37,9 +31,6 @@ export class UIPanel extends HandlebarsApplicationMixin(ApplicationV2) {
     timeChangeHandler (data) {
         this.#time = structuredClone(data.time)
         this.#time.days += 1 // display 1-based instead of 0-based
-
-        // FIXME: rendering the entire application is a terrible way to update one element when the time changes
-        // This is just a quick test.
         this.render(true)
     }
 
