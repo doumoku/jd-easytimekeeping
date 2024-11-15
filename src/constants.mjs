7d7dde223dd5df3196c97ea35333323a4f73a980
DC23 <jugglindan@gmail.com>
2024-11-15 21:09:00 +1100

    changing constants

--- src/constants.mjs
+++ src/constants.mjs
@@ -2,10 +2,11 @@
  * Defines the constants which are used in multiple places for time calculations.
  */
 
-export const Constants = {
-    minutesPerDay: 60 * 24,
-    shiftsPerDay: 4,
-    hoursPerShift: 6,
-    hoursPerDay: 24,
-    minutesPerShift: 60 * 6, // 6 hours * 60 minutes per hour
+export class Constants {
+    static minutesPerDay = 24 * 60
+    static secondsPerDay = 24 * 60 * 60
+    static shiftsPerDay = 4
+    static hoursPerShift = 6
+    static hoursPerDay = 24
+    static minutesPerShift = 6 * 60
 }
