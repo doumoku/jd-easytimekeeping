87ff9a63d0a7016dd26a4c2fd779734522604c03
DC23 <jugglindan@gmail.com>
2024-11-17 17:43:40 +1100

    Almost finished implementing the shift name settings. Just need the reset button functionality #113

--- src/shiftsettings.mjs
+++ src/shiftsettings.mjs
@@ -18,10 +18,10 @@ export function registerShiftSettings () {
         config: false,
         type: Object,
         default: {
-            'morningName': game.i18n.localize('JDTIMEKEEPING.Shift.Morning'),
-            'afternoonName': game.i18n.localize('JDTIMEKEEPING.Shift.Afternoon'),
-            'eveningName': game.i18n.localize('JDTIMEKEEPING.Shift.Evening'),
-            'nightName': game.i18n.localize('JDTIMEKEEPING.Shift.Night'),
+            morningName: game.i18n.localize('JDTIMEKEEPING.Shift.Morning'),
+            afternoonName: game.i18n.localize('JDTIMEKEEPING.Shift.Afternoon'),
+            eveningName: game.i18n.localize('JDTIMEKEEPING.Shift.Evening'),
+            nightName: game.i18n.localize('JDTIMEKEEPING.Shift.Night'),
         },
         restricted: true,
     })
@@ -40,6 +40,15 @@ class ShiftSettings extends FormApplication {
 
     getData () {
         const initialValues = game.settings.get(MODULE_ID, SETTINGS.SHIFT_SETTINGS)
+        // we can't have empty strings, so force any emptyish values back to the default
+        initialValues.morningName =
+            initialValues.morningName || game.i18n.localize('JDTIMEKEEPING.Shift.Morning')
+        initialValues.afternoonName =
+            initialValues.afternoonName || game.i18n.localize('JDTIMEKEEPING.Shift.Afternoon')
+        initialValues.eveningName =
+            initialValues.eveningName || game.i18n.localize('JDTIMEKEEPING.Shift.Evening')
+        initialValues.nightName =
+            initialValues.nightName || game.i18n.localize('JDTIMEKEEPING.Shift.Night')
         return initialValues
     }
 
