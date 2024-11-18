08e762b423608c61a3d4a81289d97afafc138fc1
Sasmira <sasmyra@gmail.com>
2024-11-18 04:16:38 +0100

    ## update french translation

---
+++ Makefile
@@ -0,0 +1,27 @@
+INPUT_DIR=./src/
+
+# For now, I only want to document functions in timekeeper.mjs
+# INPUT_FILES=$(wildcard $(INPUT_DIR)*.mjs)
+INPUT_FILES=$(INPUT_DIR)timekeeper.mjs
+
+PUBLIC_API_OPTIONS= build --access public --format md --shallow --markdown-toc-max-depth 3
+OUTPUT_DIR=./documentation/
+ALL_DOC_FILES=$(addprefix $(OUTPUT_DIR),$(addsuffix .md, $(basename $(notdir $(INPUT_FILES)))))
+
+api: $(ALL_DOC_FILES)
+
+echo:
+	@echo $(INPUT_FILES)
+	@echo
+	@echo $(ALL_DOC_FILES)
+
+lint:
+	documentation lint $(INPUT_FILES)
+
+$(OUTPUT_DIR)%.md: $(INPUT_DIR)%.mjs
+	@echo "$^ --> $@"
+	documentation $(PUBLIC_API_OPTIONS) $^ > $@
+
+.PHONY: clean
+clean:
+	rm -f $(ALL_DOC_FILES)
---
+++ src/shiftsettings.mjs
@@ -0,0 +1,76 @@
+import { Helpers } from './helpers.mjs'
+import { MODULE_ID, SETTINGS } from './settings.mjs'
+
+export function registerShiftSettings () {
+    // The settings menu
+    game.settings.registerMenu(MODULE_ID, SETTINGS.SHIFT_MENU, {
+        name: 'JDTIMEKEEPING.Settings.ShiftConfig.name',
+        label: 'JDTIMEKEEPING.Settings.ShiftConfig.label',
+        hint: 'JDTIMEKEEPING.Settings.ShiftConfig.hint',
+        icon: 'fas fa-cog',
+        type: ShiftSettings,
+        restricted: true,
+    })
+
+    // the settings object
+    game.settings.register(MODULE_ID, SETTINGS.SHIFT_SETTINGS, {
+        scope: 'world',
+        config: false,
+        type: Object,
+        default: {
+            morningName: game.i18n.localize('JDTIMEKEEPING.Shift.Morning'),
+            afternoonName: game.i18n.localize('JDTIMEKEEPING.Shift.Afternoon'),
+            eveningName: game.i18n.localize('JDTIMEKEEPING.Shift.Evening'),
+            nightName: game.i18n.localize('JDTIMEKEEPING.Shift.Night'),
+        },
+        restricted: true,
+        requiresReload: true,
+    })
+}
+
+class ShiftSettings extends FormApplication {
+    static get defaultOptions () {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            popOut: true,
+            width: 400,
+            template: `modules/${MODULE_ID}/templates/shiftsettings.hbs`,
+            id: SETTINGS.SHIFT_MENU,
+            title: 'JDTIMEKEEPING.Settings.ShiftConfig.name',
+        })
+    }
+
+    getData () {
+        const initialValues = game.settings.get(MODULE_ID, SETTINGS.SHIFT_SETTINGS)
+        // we can't have empty strings, so force any emptyish values back to the default
+        initialValues.morningName =
+            initialValues.morningName || game.i18n.localize('JDTIMEKEEPING.Shift.Morning')
+        initialValues.afternoonName =
+            initialValues.afternoonName || game.i18n.localize('JDTIMEKEEPING.Shift.Afternoon')
+        initialValues.eveningName =
+            initialValues.eveningName || game.i18n.localize('JDTIMEKEEPING.Shift.Evening')
+        initialValues.nightName =
+            initialValues.nightName || game.i18n.localize('JDTIMEKEEPING.Shift.Night')
+        return initialValues
+    }
+
+    _updateObject (event, formData) {
+        const data = foundry.utils.expandObject(formData)
+        const current = game.settings.get(MODULE_ID, SETTINGS.SHIFT_SETTINGS)
+
+        if (!Helpers.objectsShallowEqual(data, current)) {
+            game.settings.set(MODULE_ID, SETTINGS.SHIFT_SETTINGS, data)
+            SettingsConfig.reloadConfirm({ world: true })
+        }
+    }
+
+    // activateListeners (html) {
+    //     super.activateListeners(html)
+    //     html.on('click', '[data-action=reset]', this._handleResetButtonClicked)
+    // }
+
+    async _handleResetButtonClicked (event) {
+        console.log(event)
+        // const me = $(event.delegateTarget).find('[name=morningName]')
+        // me.value = game.i18n.localize('JDTIMEKEEPING.Shift.Morning')
+    }
+}
