157e700406ec6b4969eafb1fee494cbc87863fa8
DC23 <DC23@users.noreply.github.com>
2024-12-05 20:52:49 +1100

    refactoring constants class to have calculated values with the same interface #185

--- src/constants.mjs
+++ src/constants.mjs
@@ -2,14 +2,27 @@
  * Defines the constants which are used in multiple places for time calculations.
  */
 export class Constants {
+    // True constants
     static minutesPerDay = 24 * 60
     static secondsPerDay = 24 * 60 * 60
     static shiftsPerDay = 4
     static hoursPerShift = 6
     static hoursPerDay = 24
-    static minutesPerShift = 6 * 60
-    static stretchesPerShift = 24
-    static stretchesPerDay = 24 * 4
-    static minutesPerStretch = 15
+    static minutesPerShift = 6 * 60 // hoursPerShift * minutesPerHour
     static daysPerWeek = 7
+
+    // Variable and calculated values that I'm refactoring to look like the old constants
+    static get minutesPerStretch () {
+        // todo: will be a setting. Must be an even factor of 1 hour
+        return 15
+    }
+
+    // static stretchesPerShift = 24
+    static get stretchesPerShift () {
+        return Constants.minutesPerShift / Constants.minutesPerStretch
+    }
+
+    static get stretchesPerDay () {
+        return Constants.stretchesPerShift * Constants.shiftsPerDay 
+    }
 }
